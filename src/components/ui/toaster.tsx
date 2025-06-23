56ad10f591ef0bb2161f2f2d66179011245573d1
a <a@sw.com>
2025-06-23 17:29:44 -0500

    fixes para interfaz movil

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -12,18 +12,18 @@ export function Toaster() {
   const { toasts } = useToast()
 
   return (
-    <ToastProvider>
+    <ToastProvider >
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
-          <Toast key={id} {...props}>
+          <Toast key={id} {...props} className="opacity-[.96]">
             <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
+              {title && <ToastTitle className="text-md">{title}</ToastTitle>}
               {description && (
-                <ToastDescription>{description}</ToastDescription>
+                <ToastDescription className="text-md">{description}</ToastDescription>
               )}
             </div>
             {action}
-            <ToastClose />
+            <ToastClose className="!opacity-100" />
           </Toast>
         )
       })}
